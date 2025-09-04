76a00ea426d75b36c4b11bb4ab59a7f7e38dff53
sonjunho2 <sonjunho2@gmail.com>
2025-09-04 11:27:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
-import App from './App';
+import App from './src/App'; // ✅ src/App.tsx 경로 그대로 사용
 registerRootComponent(App);
