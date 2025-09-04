fbb69ee34dd2b636513efab3bc6811fc9d9f4fe5
sonjunho2 <sonjunho2@gmail.com>
2025-09-04 11:26:35 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: ['react-native-reanimated/plugin']
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo']
+    // ❌ 'react-native-reanimated/plugin' 사용 안 함
+  };
 };
