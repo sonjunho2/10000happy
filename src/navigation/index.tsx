cf742b992813243d622900645c756ab51f9bf04c
sonjunho2 <sonjunho2@gmail.com>
2025-09-05 14:33:42 +0900

    Update index.tsx

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,32 +1,16 @@
 //src/navigation/index.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FeedScreen from '../screens/FeedScreen';
-import NewPostScreen from '../screens/NewPostScreen';
-import PostScreen from '../screens/PostScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import MoneyMultiplierScreen from '../screens/MoneyMultiplierScreen';
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
-function Tabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: true }}>
-      <Tab.Screen name="게시판" component={FeedScreen} />
-      <Tab.Screen name="글쓰기" component={NewPostScreen} />
-      <Tab.Screen name="프로필" component={ProfileScreen} />
-    </Tab.Navigator>
-  );
-}
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="홈" component={Tabs} options={{ headerShown: false }} />
-        <Stack.Screen name="게시글" component={PostScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Money" component={MoneyMultiplierScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
